a67b3621725ed08f110f9186b89a54f78d3499ed
mecher_ <bradenchasefranklin@icloud.com>
2021-09-21 08:48:11 -0700

    Working EXE created and zipped

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
-from distutils.core import setup
-import py2exe
+# Note that this code is not mine.
+# Credit: https://cx-freeze.readthedocs.io/en/latest/setup_script.html
 
-setup(["main.py"])
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+build_exe_options = {"packages": ["os"]}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name = "Python Pumpkin Carver",
+    version = "1.0",
+    description = """Python Turtle Graphics Project For Ms. Woldseth's ICP1 Class.
+    Created by: Braden Franklin 
+    Period: 4
+    """,
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("main.py", base=base)]
+)
